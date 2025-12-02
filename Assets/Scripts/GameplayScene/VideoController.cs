b3cdac10f594cbf23b9eb39ffdb67afc3011f811
Vladimir Chernik <vladimir.ch@bimiboo.com>
2025-12-02 18:00:57 +0300

    added replay video button

--- Assets/Scripts/GameplayScene/VideoController.cs
+++ Assets/Scripts/GameplayScene/VideoController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.Events;
 
@@ -6,6 +7,11 @@ namespace MyGame.Gameplay
 {
     public sealed class VideoController : MonoBehaviour, IGameStage
     {
+        [SerializeField] private GameObject _buttons;
+        [SerializeField] private Button _restartButton;
+        [SerializeField] private Button _continueButton;
+
+        private ScenarioStage _scenarioStage;
         private VideoPlayer _videoPlayer;
 
         public UnityAction OnEnd { get; set; }
@@ -14,10 +20,17 @@ namespace MyGame.Gameplay
         {
             _videoPlayer = GetComponent<VideoPlayer>();
             _videoPlayer.loopPointReached += OnVideoFinished;
+            _restartButton.onClick.AddListener(() => Play(_scenarioStage));
+            _continueButton.onClick.AddListener(End);
         }
 
         public void Play(ScenarioStage scenarioStage)
         {
+            if (scenarioStage == null)
+                return;
+
+            _scenarioStage = scenarioStage;
+            _buttons.SetActive(false);
             gameObject.SetActive(true);
             _videoPlayer.clip = scenarioStage.Video;
             _videoPlayer.Play();
@@ -25,6 +38,12 @@ namespace MyGame.Gameplay
 
         private void OnVideoFinished(VideoPlayer vp)
         {
+            _buttons.SetActive(true);
+        }
+
+        private void End()
+        {
+            _buttons.SetActive(false);
             gameObject.SetActive(false);
             OnEnd?.Invoke();
         }
