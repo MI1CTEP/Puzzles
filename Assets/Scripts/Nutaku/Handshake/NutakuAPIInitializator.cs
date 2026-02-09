0fed823e7ad557b223276508fe9a6f72ad8bc435
Pavel-Masharov <leonmasharov@gmail.com>
2026-02-09 09:25:18 +0500

    new settings

--- Assets/Scripts/Nutaku/Handshake/NutakuAPIInitializator.cs
+++ Assets/Scripts/Nutaku/Handshake/NutakuAPIInitializator.cs
@@ -44,6 +44,9 @@ public class NutakuAPIInitializator : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log(Application.persistentDataPath);
+
+
         NutakuSdkConfig.loginResultToGameCallbackDelegate = LoginResultCallback;
 
 
